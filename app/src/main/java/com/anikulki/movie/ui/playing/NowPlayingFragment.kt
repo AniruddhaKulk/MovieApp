4f2c015b748faa4d9cd646997e452f54a1e54ca0
AniruddhaKulk <anikulki@gmail.com>
2020-10-24 16:58:21 +0530

    Make the api call to get list of now playing movies and observe for the result

--- app/src/main/java/com/anikulki/movie/ui/playing/NowPlayingFragment.kt
+++ app/src/main/java/com/anikulki/movie/ui/playing/NowPlayingFragment.kt
@@ -3,9 +3,11 @@ package com.anikulki.movie.ui.playing
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.anikulki.movie.R
 import com.anikulki.movie.databinding.FragmentNowPlayingBinding
+import com.anikulki.movie.utils.common.State
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
@@ -17,6 +19,8 @@ class NowPlayingFragment: Fragment(R.layout.fragment_now_playing){
     private val binding
         get() = _binding!!
 
+    private val viewModel by viewModels<NowPlayingViewModel>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -27,6 +31,28 @@ class NowPlayingFragment: Fragment(R.layout.fragment_now_playing){
 
         binding.apply {
             rvNowPlaying.addItemDecoration(dividerItemDecoration)
+        }
+
+        viewModel.getNowPlaying()
+
+        viewModel.nowPlayingLiveData.observe(viewLifecycleOwner){state ->
+            when(state){
+                is State.Loading -> binding.swipeRefresh.isRefreshing = true
+
+                is State.Success -> {
+                    binding.swipeRefresh.isRefreshing = false
+                }
+
+                is State.Error -> {
+                    //show the error view here
+                    binding.swipeRefresh.isRefreshing = false
+                }
+            }
+
+        }
+
+        binding.swipeRefresh.setOnRefreshListener {
+            viewModel.getNowPlaying()
         }
     }
 
