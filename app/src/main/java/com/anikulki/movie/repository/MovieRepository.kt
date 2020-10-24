86acdea2adab55e867371ee870c1e1e84a80a257
AniruddhaKulk <anikulki@gmail.com>
2020-10-24 16:49:26 +0530

    Use repository to make the api call and save the movies into database.

---
+++ app/src/main/java/com/anikulki/movie/repository/MovieRepository.kt
@@ -0,0 +1,59 @@
+package com.anikulki.movie.repository
+
+import com.anikulki.movie.data.local.db.DatabaseService
+import com.anikulki.movie.data.local.db.entity.Movie
+import com.anikulki.movie.data.remote.NetworkService
+import com.anikulki.movie.data.remote.response.MoviesResponse
+import com.anikulki.movie.utils.common.State
+import dagger.Lazy
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import retrofit2.Response
+import javax.inject.Inject
+import javax.inject.Singleton
+
+
+@ExperimentalCoroutinesApi
+@Singleton
+class MovieRepository @Inject constructor(
+    private val networkService: Lazy<NetworkService>,
+    private val databaseService: DatabaseService
+){
+
+    fun getNowPlaying(): Flow<State<List<Movie>>>{
+        return object: NetworkBoundRepository<List<Movie>, MoviesResponse>(){
+
+            override suspend fun getFromRemoteServer(): Response<MoviesResponse> {
+                return networkService.get().callNowPlaying()
+            }
+
+            override fun getFromDatabase(): Flow<List<Movie>> {
+                return databaseService.movieDao().getNowPlayingMovies()
+            }
+
+            override suspend fun saveRemoteData(response: MoviesResponse) {
+                val nowPlayingList = movieMapper(response)
+                databaseService.movieDao().insertNowPlayingMovies(nowPlayingList)
+            }
+
+        }.asFlow()
+    }
+
+    private fun movieMapper(moviesResponse: MoviesResponse): List<Movie>{
+        val list = mutableListOf<Movie>()
+
+        moviesResponse.results.forEach {
+            val movie = Movie(
+                it.id.toLong(),
+                it.title,
+                it.release_date,
+                it.poster_path,
+                it.overview,
+            )
+
+            list.add(movie)
+        }
+
+        return list
+    }
+}
--- app/src/main/java/com/anikulki/movie/ui/playing/NowPlayingViewModel.kt
+++ app/src/main/java/com/anikulki/movie/ui/playing/NowPlayingViewModel.kt
@@ -1,10 +1,33 @@
 package com.anikulki.movie.ui.playing
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.anikulki.movie.data.local.db.entity.Movie
+import com.anikulki.movie.repository.MovieRepository
+import com.anikulki.movie.utils.common.State
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 
 @ExperimentalCoroutinesApi
-class NowPlayingViewModel: ViewModel(){
+class NowPlayingViewModel @androidx.hilt.lifecycle.ViewModelInject constructor(
+    private val repository: MovieRepository
+): ViewModel(){
 
+    private val _nowPlayingLiveData = MutableLiveData<State<List<Movie>>>()
+
+    val nowPlayingLiveData: LiveData<State<List<Movie>>>
+        get() = _nowPlayingLiveData
+
+
+    fun getNowPlaying(){
+        viewModelScope.launch {
+            repository.getNowPlaying().collect {
+               _nowPlayingLiveData.value = it
+            }
+        }
+    }
 }
